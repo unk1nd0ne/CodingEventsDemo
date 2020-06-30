07c343a76c6af538518355460dae1c6e3b108dae
unk1nd0ne <z.neil.schaefer@gmail.com>
2020-06-30 11:44:26 -0500

    done

---
+++ CodingEventsDemo/Models/EventType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.Models
+{
+    public enum EventType
+    {
+        Conference,
+        Meetup,
+        Workshop,
+        Social
+
+    }
+
+
+}
--- CodingEventsDemo/ViewModels/AddEventViewModel.cs
+++ CodingEventsDemo/ViewModels/AddEventViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace CodingEventsDemo.ViewModels
@@ -15,5 +18,16 @@ namespace CodingEventsDemo.ViewModels
 
         [EmailAddress]
         public string ContactEmail { get; set; }
+
+        public EventType Type { get; set; }
+
+        public List<SelectListItem> EventTypes { get; set; } = new List<SelectListItem>
+        {
+            new SelectListItem(EventType.Conference.ToString(), ((int)EventType.Conference).ToString()),
+            new SelectListItem(EventType.Meetup.ToString(), ((int)EventType.Meetup).ToString()),
+            new SelectListItem(EventType.Social.ToString(), ((int)EventType.Social).ToString()),
+            new SelectListItem(EventType.Workshop.ToString(), ((int)EventType.Workshop).ToString())
+        };
+
     }
 }
