6ef3689934f4cedc00696c11860752eeb01d8507
unk1nd0ne <z.neil.schaefer@gmail.com>
2020-07-10 08:06:54 -0500

    Added tags

---
+++ CodingEventsDemo/Controllers/TagController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
+
+namespace CodingEventsDemo.Controllers
+{
+    public class TagController : Controller
+    {
+        private EventDbContext context;
+
+        public TagController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Tag> eventTags = context.Tags.ToList();
+
+            return View(eventTags);
+        }
+
+        public IActionResult Create()
+        {
+            AddTagViewModel addTagViewModel = new AddTagViewModel();
+
+            return View(addTagViewModel);
+        }
+
+        [HttpPost]
+        [Route("Tag/Create")]
+        public IActionResult ProcessCreateTagFormt(AddTagViewModel addTagViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Tag newTag = new Tag
+                {
+                    Name = addTagViewModel.Name,
+                };
+
+                context.Tags.Add(newTag);
+                context.SaveChanges();
+
+                return Redirect("/Tag");
+            }
+
+            return View("Create", addTagViewModel);
+        }
+    }
+}
--- CodingEventsDemo/Models/Tag.cs
+++ CodingEventsDemo/Models/Tag.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CodingEventsDemo.Models
 {
     public class Tag
     {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+
+        public List<Event> Events { get; set; }
+
+        public Tag()
+        {
+        }
+
+        public Tag(string name)
+        {
+            Name = name;
+        }
     }
 }
